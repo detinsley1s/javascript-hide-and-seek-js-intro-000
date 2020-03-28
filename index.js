750347a9c7093e609b72a36e3e34e617244a895e
funny-teacher-0837 <detinsley1s@gmail.com>
2020-03-28 01:25:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function nestedTarget() {
   return document.getElementById('nested')
     .querySelector('.target')
 }
+
+function increaseRankBy(n) {
+  const l = document.querySelectorAll('.ranked-list li')
+  for(let i = 0; i < l.length; ++i) {
+    l[i].innerHTML = parseInt(l[i].innerHTML) + n
+  }
+}
